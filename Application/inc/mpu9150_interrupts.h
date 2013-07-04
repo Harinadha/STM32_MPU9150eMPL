1198ac14e021742e00f1e37d1393952c2d677a54
Harinadha <harinath.cau@gmail.com>
2013-07-04 12:14:04 +0900

    added few comments

--- Application/inc/mpu9150_interrupts.h
+++ Application/inc/mpu9150_interrupts.h
@@ -1,3 +1,31 @@
+//MPU9150 I2C library for ARM STM32F103xx Microcontrollers - Header file has defines
+// to choose MPU9150/MPU6050 INT pin. 
+// 25/06/2013 by Harinadha Reddy Chintalapalli <harinath.ec@gmail.com>
+// Changelog:
+//     2013-06-25 - Initial release.
+/* ============================================================================================
+MPU9150 device I2C library code for ARM STM32F103xx is placed under the MIT license
+Copyright (c) 2013 Harinadha Reddy Chintalapalli
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================================================================
+*/
 #ifndef __MPU9150_INTERRUPTS_H
 #define __MPU9150_INTERRUPTS_H
 
--- Application/src/mpu9150_interrupts.c
+++ Application/src/mpu9150_interrupts.c
@@ -1,3 +1,31 @@
+//MPU9150 I2C library for ARM STM32F103xx Microcontrollers - Source file
+//Has functions for configuring MPU9150/MPU6050 INT pin.
+// 25/06/2013 by Harinadha Reddy Chintalapalli <harinath.ec@gmail.com>
+// Changelog:
+//     2013-06-25 - Initial release. Thanks to Invensense for releasing MPU driver for msp430.
+/* ============================================================================================
+MPU9150 device I2C library code for ARM STM32F103xx is placed under the MIT license
+Copyright (c) 2013 Harinadha Reddy Chintalapalli
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+================================================================================================
+*/
 #include "mpu9150_interrupts.h"
 
 void MPU9150_Interrupt_Init(MPU9150int_TypeDef MPU9150int, MPU9150intMode_TypeDef MPU9150int_Mode)
